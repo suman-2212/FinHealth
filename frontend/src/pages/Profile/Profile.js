abd6dcc15cc0b5cb78a9d038a269261dc241f9cd
suman-2212 <11suman12m@gmail.com>
2026-02-06 21:03:30 +0530

    Fix ESLint error: remove unused useAuth import

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import {
